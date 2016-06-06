964e01a659a5a31191d5e545447a8549d0d3a333
Dan Hoying <danhoying@gmail.com>
2016-06-06 08:11:26 -0700

    Add descriptions and finish triangle.js

---
+++ chessboard.js
@@ -0,0 +1,15 @@
+// Chess Board
+
+// Write a program that creates a string that represents an 8×8 grid, using
+// new-line characters to separate lines. At each position of the grid there
+// is either a space or a “#” character. The characters should form a chess
+// board. Passing this string to console.log should show something like this:
+
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+// # # # #
---
+++ triangle.js
@@ -0,0 +1,16 @@
+// Looping a Triangle
+
+// Write a loop that makes seven calls to console.log to output the following
+// triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for (var symbol = '#'; symbol.length < 8; symbol += '#') {
+	console.log(symbol);
+}
