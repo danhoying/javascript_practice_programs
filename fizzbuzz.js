1da355eace2a6275a95aa5393e6bd697c268f367
Dan Hoying <danhoying@gmail.com>
2016-06-07 07:54:09 -0700

    Finish FizzBuzz exercise

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,3 +5,18 @@
 // number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 // When you have that working, modify your program to print "FizzBuzz"
 // for numbers that are divisible by both 3 and 5.
+
+for (var num = 1; num <= 100; num ++) {
+    if (num % 5 == 0 && num % 3 == 0) {
+		console.log("FizzBuzz");
+	}
+	else if (num % 3 == 0) {
+		console.log("Fizz");
+	}
+	else if (num % 5 == 0) {
+		console.log("Buzz");
+	}
+	else {
+	console.log(num);
+	}
+}
