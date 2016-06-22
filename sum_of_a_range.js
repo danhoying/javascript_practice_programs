71b19dbf096aa9b1c50b265af0a9d95261b1d1bd
Dan Hoying <danhoying@gmail.com>
2016-06-22 15:06:31 -0700

    Finish sum of range exercise

--- sum_of_a_range.js
+++ sum_of_a_range.js
@@ -11,3 +11,39 @@
 // one, correspond- ing to the old behavior. The function call range(1, 10, 2)
 // should return [1, 3, 5, 7, 9] . Make sure it also works with negative step
 // values so that range(5, 2, -1) produces [5, 4, 3, 2].
+
+function range(start, end, step) {
+    var rangeArray = [];
+
+    if (arguments.length == 3) {
+	if (step > 0) {
+	    for (var i = start; i <= end; i = i + step) {
+		rangeArray.push(i);
+	    }
+	}
+	else
+	    for (var i = start; i >= end; i = i + step) {
+		rangeArray.push(i);
+	    }
+    }
+    else {
+	for (var i = start; i <= end; i++) {
+	    rangeArray.push(i);	
+	}
+    }
+    return rangeArray;
+} 
+
+function sum(numArray) {
+    var arraySum = 0;
+    for (var i = 0; i < numArray.length; i++) {
+	var num = numArray[i];
+	arraySum = arraySum + num;
+    }
+    return arraySum; 
+} 
+
+console.log(range(3, 25));
+console.log(range(1, 10, 2));
+console.log(range(5, 2, -1));
+console.log(sum([1,2,3,4,5,6,7,8,9,10]));
