37ba4a085b7facd32bbd36ccaf44254d34070c02
Dan Hoying <danhoying@gmail.com>
2016-08-26 12:08:12 -0700

    Add confirm_ending, repeat_string, and truncate_string programs

---
+++ confirm_ending.js
@@ -0,0 +1,16 @@
+// Check if a string (first argument, str) ends with the given target string 
+// (second argument, target).
+// This challenge can be solved with the .endsWith() method, which was 
+// introduced in ES2015. But for the purpose of this challenge, we would like 
+// you to use one of the JavaScript substring methods instead.
+
+function confirmEnding(str, target) {
+  for (var i = 0; i < str.length; i++) {
+    if (str.substring(str.length - i) === target) {
+      return true;
+    }
+  }
+  return false;
+}
+
+confirmEnding("Bastian", "n");
